a251a9942b94b3bf8b44f58cb8c8cfcd6b44e2f5
Angel Prado <angel_joaquin12@outlook.com>
2023-04-13 13:53:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let garage = document.getElementById("garage");
+const list = document.createElement("button");
+let cars = document.getElementById("addCar");
+cars.addEventListener("click", boton);
+//cars.addEventListener("dblclick", borarBoton );
+//let btn = document.createElement("button");
+//btn.innerHTML = "Submit";
+//btn.type = "submit";
+//btn.name = "formBtn";
+//document.body.appendChild(btn);
+
+function boton() {
+  let btn = document.createElement("button");
+  btn.innerText = "New Car";
+  btn.type = "submit";
+  btn.name = "formBtn";
+  document.body.appendChild(btn);
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "X";
+  deleteButton.style.color = "white";
+  deleteButton.style.backgroundColor = "red";
+  //deleteButton.style.boxShadow = "2px 5px black";
+  document.body.appendChild(deleteButton);
+  deleteButton.addEventListener("click", function () {
+    btn.remove();
+    deleteButton.remove();
+  });
+  
+  btn.addEventListener("click", function () {
+    document.body.querySelector(".btn");
+    //btn.textContent='New Text';
+    btn.textContent='New Text';
+    console.log("ye");
+  });
+}
+//function borarBoton (event) {
+// let btn = document.getElementById("button");
+// btn.remove ();
+//  console.log("1");
+//}
