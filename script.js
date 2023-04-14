079c17c952022d0d303f1ff356f89468683c91d3
Angel Prado <angel_joaquin12@outlook.com>
2023-04-14 08:03:02 -0500

    swapped text input/buttons

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ cars.addEventListener("click", boton);
 //document.body.appendChild(btn);
 
 function boton() {
-  let btn = document.createElement("button");
-  btn.innerText = "New Car";
-  btn.type = "submit";
-  btn.name = "formBtn";
+  let btn = document.createElement("input");
+  btn.value = ("New Car");
+  btn.type = "text";
+  //btn.name = "formBtn";
   document.body.appendChild(btn);
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "X";
@@ -21,15 +21,15 @@ function boton() {
   deleteButton.style.backgroundColor = "red";
   //deleteButton.style.boxShadow = "2px 5px black";
   document.body.appendChild(deleteButton);
-  deleteButton.addEventListener("click", function () {
+  deleteButton.addEventListener("click", () => {
     btn.remove();
     deleteButton.remove();
   });
   
-  btn.addEventListener("click", function () {
-    document.body.querySelector(".btn");
+  btn.addEventListener("click", () => {
+    //document.body.querySelector(".btn");
     //btn.textContent='New Text';
-    btn.textContent='New Text';
+    btn.value=( '');
     console.log("ye");
   });
 }
